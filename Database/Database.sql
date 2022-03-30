1ef781e288f20715fccf63040daa4bf8044b5c48
El7brouk <omar.7brouk@gmail.com>
2022-03-30 21:08:44 +0200

    Basic DB structure

---
+++ Database/Database.sql
@@ -0,0 +1,44 @@
+create database Eastern_Union;
+Use Eastern_Union;
+
+create table Users  (
+NationalID integer primary key,
+Name text not null,
+phone text not null,
+Email text not null,
+Birth_Date DATE not null,
+Country	text not null
+);
+	
+create table Employess  (
+NationalID integer primary key,
+Name text not null,
+phone text not null,
+Email text not null,
+Birth_Date DATE not null,
+Country	text not null,
+Password text not null,
+Job ENUM('Cashier','Admin') not null
+);
+
+create table Accounts  (
+ID integer not null ,
+UserID integer not null,
+Credits integer not null
+);
+
+create table Exchange  (
+ID integer not null,
+Name_of_Currency text not null,
+Value integer not null
+);
+
+create table Transactions  (
+ID integer not null,
+FromID integer not null,
+ToID integer not null,
+Process_Type ENUM('Withdrawl',' Deposit','Exchange', 'Transfer') not null
+);
+
+
+
